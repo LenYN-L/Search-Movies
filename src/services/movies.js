a3d3751d02754ab491875e580a687a0f89843f6d
lenyn <lenyn-12-31@outlook.com>
2023-04-12 11:51:53 -0500

    corrigiendo errores

--- src/services/movies.js
+++ src/services/movies.js
@@ -1,8 +1,10 @@
+const apiKey = 'd25392eb'
+
 export const searchMovies = async ({ search }) => {
   if (search === '') return null
 
   try {
-    const res = await fetch(`http://www.omdbapi.com/?apikey=d25392eb&s=${search}`)
+    const res = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${search}`)
     const json = await res.json()
     const movies = json.Search
     return movies?.map(movie => ({
